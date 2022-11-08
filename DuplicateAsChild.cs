7a1cc9ab77aff5ab659ac26c7e60eccbc6aa2a60
JustSleightly <nsteven22@gmail.com>
2022-11-07 20:17:44 -0800

    Fixed Right-Click GameObject Context Menu

--- DuplicateAsChild.cs
+++ DuplicateAsChild.cs
@@ -7,21 +7,30 @@ namespace JustSleightly
     public class DuplicateAsChild : MonoBehaviour
     {
         [MenuItem("GameObject/Duplicate As Child %#d", false, -753)] //Add to GameObject Context Menu
-        private static void DupeAsChild()
+        public static void DupeAsChild(MenuCommand command)
         {
+            var targetObject = command.context as GameObject; //Detect if this was clicked from right-click context menu
             var selectedObjects = Selection.GetFiltered<GameObject>(SelectionMode.Editable); //Locally Store Selected Editable Objects
 
             for (var i = 0; i < selectedObjects.Length; i++) //For Each Selected Editable Object
-                if (selectedObjects[i] != null) //Check if Selection Exists
-                {
-                    var newChild = Instantiate(selectedObjects[i], Vector3.zero, Quaternion.identity, selectedObjects[i].transform); //Duplicate GameObject and Set Transform/Parent
-                    newChild.name = selectedObjects[i].name; //Set Name of Child
-                    Undo.RegisterCreatedObjectUndo(newChild, "Duplicate " + selectedObjects[i].name + " as Child"); //Add Undo Record
-
-                    selectedObjects[i] = newChild; //Replace Selection with New Duplicate
-                }
+                if (targetObject == null) //If using top toolbar context menu or keyboard shortcut
+                    selectedObjects[i] = DupeChild(selectedObjects[i]); //Replace Selection with New Duplicate of All
+                else if (selectedObjects[i] == targetObject) //If using right-click context menu on GameObject
+                    selectedObjects[i] = DupeChild(targetObject); //Replace Selection with New Duplicate of Self
 
             Selection.objects = selectedObjects; //Change Selection to New Objects
+        }
+        public static GameObject DupeChild(GameObject obj)
+        {
+            if (obj != null) //Check if Selection Exists
+            {
+                var newChild = Instantiate(obj, Vector3.zero, Quaternion.identity, obj.transform); //Duplicate GameObject and Set Transform/Parent
+                newChild.name = obj.name; //Set Name of Child
+                Undo.RegisterCreatedObjectUndo(newChild, "Duplicate " + obj.name + " as Child"); //Add Undo Record
+                return newChild; //Return Child
+            }
+            else
+                return null; //Ignore
         }
     }
 }
